b52c65df8715818e2e90d2875fe4c2f9e1007493
sanjib292-cmd <sanjibsah919@gmail.com>
2022-04-22 18:39:32 +0530

    new

--- .history/config/custom-environment-variables_20220422141209.js
+++
@@ -1,8 +0,0 @@
-{
-    "serviceId": "foodie_serviceId",   
-    "accountSid": "foodie_accountSid",  
-    "authToken": "foodie_authToken", 
-    "JwtPrivatekey": "jwtPrivatekey",
-    "db": "foodie_db",
-    "db_password":"Db_pass"  
-}
